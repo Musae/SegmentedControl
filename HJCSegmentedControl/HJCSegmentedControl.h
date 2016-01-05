f25ea692d9b28b48b5e5c2043ed3a76e82759d23
Jasmine He <jasminehe@cmcchj.local>
2016-01-05 17:12:36 +0800

    Initial Commit

---
+++ HJCSegmentedControl/HJCSegmentedControl.h
@@ -0,0 +1,13 @@
+//
+//  HJCSegmentedControl.h
+//  HJCSegmentedControl
+//
+//  Created by Jasmine He on 16/1/5.
+//  Copyright © 2016年 Jasmine He. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HJCSegmentedControl : NSObject
+
+@end
---
+++ HJCSegmentedControl/HJCSegmentedControl.m
@@ -0,0 +1,13 @@
+//
+//  HJCSegmentedControl.m
+//  HJCSegmentedControl
+//
+//  Created by Jasmine He on 16/1/5.
+//  Copyright © 2016年 Jasmine He. All rights reserved.
+//
+
+#import "HJCSegmentedControl.h"
+
+@implementation HJCSegmentedControl
+
+@end
